393e6b1b3999f575459d5266a4e07855c1e93992
Lindsay Ward <lward23@hotmail.com>
2021-12-11 19:36:17 -0800

    Added default loading word

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -3,36 +3,49 @@ import React, { useState } from "react";
 import Result from "./Result";
 import "./Dictionary.css";
 
-export default function Dictionary() {
-  let [word, setWord] = useState("");
+export default function Dictionary(props) {
+  let [word, setWord] = useState(props.defaultWord);
   let [results, setResults] = useState(null);
+  let [load, setLoaded] = useState(false);
 
+  function loading() {
+    search();
+    setLoaded(true);
+  }
   function handleResponse(response) {
     setResults(response.data[0]);
   }
 
-  function search(event) {
-    event.preventDefault();
+  function search() {
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
     axios.get(apiUrl).then(handleResponse);
   }
 
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
   function handleWordChange(event) {
     event.preventDefault();
     setWord(event.target.value);
   }
 
-  return (
-    <div className="Dictionary">
-      <form onSubmit={search}>
-        <input
-          type="search"
-          autoFocus={true}
-          placeholder="Enter a word..."
-          onChange={handleWordChange}
-        />
-      </form>
-      <Result results={results} />
-    </div>
-  );
+  if (load) {
+    return (
+      <div className="Dictionary">
+        <form onSubmit={handleSubmit}>
+          <input
+            type="search"
+            autoFocus={true}
+            placeholder="Enter a word..."
+            onChange={handleWordChange}
+          />
+        </form>
+        <Result results={results} />
+      </div>
+    );
+  } else {
+    loading();
+    return <div>"Loading..."</div>;
+  }
 }
